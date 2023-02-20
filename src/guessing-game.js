4e5cfc55f14e999950b7724d7e00ea6414ff0b32
checas1 <checas104@gmail.com>
2023-02-20 17:55:01 +0200

    feat: finish class colution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,20 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+      this.max = max
+      this.min = min
     }
 
     guess() {
-
+      return this.completion = Math.ceil((this.max + this.min) / 2)
     }
 
     lower() {
-
+      this.max = this.completion
     }
 
     greater() {
-
+      this.min = this.completion
     }
 }
 
-module.exports = GuessingGame;
+module.exports = GuessingGame
